7cd89264186a39515736318c42dedcbc3000aea4
riyaskh123 <riyaskh123@gmail.com>
2026-08-20 10:34:14 +0400

    logo layout

--- src/pages/DisplayScreen.jsx
+++ src/pages/DisplayScreen.jsx
@@ -34,7 +34,7 @@ export default function DisplayScreen() {
   return (
     <div className="min-h-screen flex flex-col relative overflow-hidden">
       <video src={video} autoPlay loop muted className="absolute z-0 top-0 left-0 w-full h-full object-cover opacity-25" />
-      <img src={logo} alt="logo" className={`absolute top-[20%] w-[150px] translate-x-[-50%]  md:top-[50%] ${availableCount === 0 ? 'md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:w-[300px] opacity-50' : 'left-[100px] w-[200px]'} translate-y-[-50%] `} />
+      <img src={logo} alt="logo" className={`absolute top-[20%] w-[150px] translate-x-[-50%]  md:top-[50%] ${availableCount === 0 ? 'md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:w-[300px] opacity-50' : 'left-[150px] w-[200px]'} translate-y-[-50%]`} />
 
       {availableCount === 0 &&
         <div className='absolute top-20 left-4 z-50 w-full overflow-auto'>
